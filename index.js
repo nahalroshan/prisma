a05ad52b83f3b030fb12887bd370a9df6fe182ca
Nahal Roshan K <nahalroshan5@gmail.com>
2024-05-14 08:41:29 +0530

    Delete operation

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const { PrismaClient } = require("@prisma/client");
 
-app.use(express.json())
+const prisma = new PrismaClient();
 
-app.listen(3001,()=> console.log(`Server running port ${3001}`))
+app.use(express.json());
+
+// CRUD -> Create, Read, Update
+app.get("/", async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
+});
+
+app.post("/", async (req, res) => {
+  const newUser = await prisma.user.create({ data: req.body });
+  res.json(newUser);
+});
+
+app.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const newAge = req.body.age;
+  const newUser = await prisma.user.update({
+    where: { id: parseInt(id) },
+    data: { age: newAge },
+  });
+  res.json(newUser);
+});
+
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const deletedUser = await prisma.user.delete({ where: { id: parseInt(id) } });
+  res.json(deletedUser);
+});
+app.listen(3001, () => console.log(`Server running port ${3001}`));
